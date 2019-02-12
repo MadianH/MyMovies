c3ede5fe3c9fd7335b158b45a68b9a6fdd6cff9c
MadianH <madianh33@gmail.com>
2019-02-12 14:53:43 +0100

    [REFACTOR] : ajout du fichier login.js pour stocker les données sensibles

---
+++ backend/login.js
@@ -0,0 +1,3 @@
+module.exports = {
+   keyMovieDatabase: 'ed9604b3d9b898d05c961512789f22d3'
+}
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,14 +2,18 @@ var express = require('express');
 var router = express.Router();
 // le module request permet à l'API de pouvoir request une autre API
 var request = require('request');
+// import les logins sensibles
+var login = require("../login.js");
+
 
 /* GET movieList*/
 router.get('/movieList', function(req, res, next) {
   let movieList;
   // Intérroge l'API Movie database
-  request("https://api.themoviedb.org/3/discover/movie?vote_count.lte.gte=5&release_date.lte=2019-01-01&api_key=ed9604b3d9b898d05c961512789f22d3&language=fr&region=fr", function(error, response, body) {
-    movieList = JSON.parse(body);
-    res.json({ movieList: movieList });
+  request(`https://api.themoviedb.org/3/discover/movie?vote_count.lte.gte=5&release_date.lte=2019-01-01&api_key=${login.keyMovieDatabase}&language=fr&region=fr`,
+     function(error, response, body) {
+       movieList = JSON.parse(body);
+       res.json({ movieList: movieList });
   });
 });
 
