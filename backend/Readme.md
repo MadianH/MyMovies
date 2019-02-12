32cc23eb8cb7336d1143f1c65fe8177723930d65
MadianH <madianh33@gmail.com>
2019-02-12 13:38:42 +0100

    [BUILD] : ajout du module request

---
+++ backend/Readme.md
@@ -0,0 +1,16 @@
+# MyMovies
+  Mymovies est une application web qui permet de se créer une liste de films favoris.
+  Une liste de films vous ai proposé sur la page d'accueil, vous pouvez parcourir la liste
+  et "liker" les films. Si un film est "liker" vous le retrouverai dans vos favoris.
+
+
+## Stack
+  * ExpressJs
+  * Request
+  * CRUD
+
+## Web service
+  * Movie database
+
+## Tâches
+1.  
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+// le module request permet à l'API de pouvoir request une autre API
+var request = require('request');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+/* GET movieList*/
+router.get('/movieList', function(req, res, next) {
+  let movieList;
+  // Intérroge l'API Movie database
+  request("https://api.themoviedb.org/3/discover/movie?vote_count.lte.gte=5&release_date.lte=2019-01-01&api_key=ed9604b3d9b898d05c961512789f22d3&language=fr&region=fr", function(error, response, body) {
+    movieList = JSON.parse(body);
+    res.json({ movieList: movieList });
+  });
 });
 
 module.exports = router;
