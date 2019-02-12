cbdd10816d1cce3f8b640697cebe63e385e805d7
MadianH <madianh33@gmail.com>
2019-02-12 16:08:02 +0100

    [FEAT] : ajout de la fonction dateNow qui retourne la date actuelle et intéroge l'api MovieDataBase avec celle ci

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -2,19 +2,41 @@ var express = require('express');
 var router = express.Router();
 // le module request permet à l'API de pouvoir request une autre API
 var request = require('request');
-// import les logins sensibles
+// import les donnèes sensibles
 var login = require("../login.log");
+
+// return la date d'aujourd'hui au format anglophone
+var dateNow = function () {
+  let newDate = new Date()
+  let year = newDate.getFullYear()
+  let month = newDate.getMonth() + 1
+  let day = newDate.getDate()
+  let date = year + "-" + month + "-" + day
+  return date
+}
 
 
 /* GET movieList*/
 router.get('/movieList', function(req, res, next) {
   let movieList;
+  let newDate = dateNow()
   // Intérroge l'API Movie database
-  request(`https://api.themoviedb.org/3/discover/movie?vote_count.lte.gte=5&release_date.lte=2019-01-01&api_key=${login.keyMovieDatabase}&language=fr&region=fr`,
+  request(`https://api.themoviedb.org/3/discover/movie?vote_count.lte.gte=5&release_date.lte=${newDate}&api_key=${login.keyMovieDatabase}&language=fr&region=fr`,
      function(error, response, body) {
        movieList = JSON.parse(body);
        res.json({ movieList: movieList });
   });
 });
+
+// // GET likeMovieList
+// router.get('/likemovieList', function(req, res, next) {
+//   let likeMovieList;
+//   // Intérroge l'API Movie database
+//   request(`https://api.themoviedb.org/3/discover/movie?vote_count.lte.gte=5&release_date.lte=${newDate}&api_key=${login.keyMovieDatabase}&language=fr&region=fr`,
+//      function(error, response, body) {
+//        movieList = JSON.parse(body);
+//        res.json({ movieList: movieList });
+//   });
+// });
 
 module.exports = router;
