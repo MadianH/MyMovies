3f3bba66dbff4417eb9517e264cb29f2408cbabb
MadianH <madianh33@gmail.com>
2019-02-13 14:43:07 +0100

    [FEAT] : ajout du composant Card ce composant accueille les films

--- frontend/src/Card.js
+++ frontend/src/Card.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import { Card, CardImg, CardText, CardBody,CardTitle, CardSubtitle } from 'reactstrap';
 import './App.css';
+
 
 class Card extends Component {
   render() {
     return (
-      <div className="App">
-        
+      <div>
+        <Card>
+          <CardImg top width="100%" src={this.props.img} alt="Card image cap" />
+          <CardBody>
+            <CardTitle>{this.props.title}</CardTitle>
+            <CardSubtitle>Card subtitle</CardSubtitle>
+            <CardText>{this.props.overview}</CardText>
+          </CardBody>
+        </Card>
       </div>
     );
   }
